d9926282825dd6b532eb6832c5f199395ae51257
Dimitar Dimitrov <mitkodimitar@gmail.com>
2015-02-23 19:55:15 -0500

    threshold voltage and whathaveyou

--- mdma.py
+++ mdma.py
@@ -8,31 +8,43 @@ def linspace(initial, final, n = 100): #Creds to Brad Minch
     else:
         return []
 
-s = smu.smu()
-s.set_current(2, 0)
+vt=2.1
 v = linspace(0, 5, 101)
 i = linspace(0, 5, 101)
 
 plt.ion()
-lline = plt.plot(v,i,'g--')[0]
+rline = plt.plot(v,i,'g--')[0]
 graph = plt.plot(v,i)[0]
 point = plt.plot(v[0],i[0],'ob')[0]
 xlabl = plt.xlabel('$V_D$',fontsize=24)
 ylabl = plt.ylabel('$I_D$',fontsize=24)
 vgtxt = plt.text(.5,4.5,'$V_G=$ ',fontsize=24)
+title = plt.title('IV Charecteristic of BS170 nMOSFET',fontsize=24)
+
+s = smu.smu() #Creds to Brad Minch
+s.set_current(2, 0)
 
 run=True
-
 while run:
     for n in range(len(v)):
-        s.set_voltage(1, v[n])
-        s.autorange(1)
-        i[n]=1000*s.get_current(1)
-        vg=s.get_voltage(2)
-        vgtxt.set_text('$V_G=$ '+str(vg))
+        try:
+            s.set_voltage(1, v[n])
+            s.autorange(1)
+            i[n]=1000*s.get_current(1)
+            vg=s.get_voltage(2)
+        except:
+            print "Device disconnected"
+            run = False
+            break
+        off=vg<vt
+        vgtxt.set_text('$V_G=$ '+str(vg)+' > '*(not off)+' < '*off+str(vt))
+        vgtxt.set_color('r'*off+'g'*(not off))
         graph.set_ydata(i)
         point.set_xdata(v[n])
         point.set_ydata(i[n])
         plt.draw()
-
-s.set_voltage(1, 0.)
+plt.text(1,2.5,'DEVICE DISCONNECTED',fontsize=24,color='r')
+plt.draw()
+from time import sleep
+sleep(3)
+#s.set_voltage(1, 0.)
